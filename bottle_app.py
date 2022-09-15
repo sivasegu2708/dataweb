b1aa59b0669b7204a193d55a924f61b79ff75a47
Greg <gregdelozier@gmail.com>
2022-09-14 21:45:24 -0400

    first web app with data

--- bottle_app.py
+++ bottle_app.py
@@ -1,10 +1,27 @@
+from bottle import default_app, route, template
+import sqlite3
 
-# A very simple Bottle Hello World app for you to get started with...
-from bottle import default_app, route
+connection = sqlite3.connect("shopping_list.db")
 
 @route('/')
 def hello_world():
     return 'Hello from Dr. DeLozier!'
+
+@route('/hi')
+def hi_world():
+    return 'Hi from Dr. DeLozier!'
+
+@route('/bye')
+def bye_world():
+    return 'Bye from Dr. DeLozier!'
+
+@route('/list')
+def get_list():
+    cursor = connection.cursor()
+    rows = cursor.execute("select id, description from list")
+    rows = list(rows)
+    rows = [ {'id':row[0] ,'desc':row[1]} for row in rows ]
+    return template("shopping_list.tpl", name="Dr. DeLozier", shopping_list=rows)
 
 application = default_app()
 
